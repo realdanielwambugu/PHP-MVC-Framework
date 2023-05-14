4b66eae4ed8794f6a82102d7f7174e56d0580e7d
daniel <pirplanet@gmail.com>
2023-05-14 19:31:49 +0300

    update

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -12,20 +12,9 @@ class HomeController
 {
     public function show(Request $request, Response $response)
    {   
-      // if (Gate::allows('admin-only', Auth::User())) 
-      // {
-            if ($request->getPath() === 'analytics') 
-            {
-              return $response->withView(
-                'admin/includes/analytics'
-              );
-            }
-            
-            return $response->withView('admin/dashboard', []);
-      // }
-
+  
       return $response->withView(
-        'welcome/home'
+        '/home'
       );
 
     }
